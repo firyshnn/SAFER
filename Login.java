a6e2eb2dea25f17ec8c3b6c2041c548ddc8a92cd
HnaCvs <24-37176@g.batstate-u.edu.ph>
2025-10-23 15:23:20 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,1 +1,19 @@
+import java.util.Scannner;
 
+public class Login {
+  public static void main (String [] args){
+    Scanner sc = new Scanner (System.in);
+    String username;
+    String password;
+
+    System.out.println("Enter username: ");
+    username = sc.nextLine();
+    System.out.println("Enter password: ");
+    password = sc.nextLine();
+
+    while (password != "12345678"){
+      System.println("Incorrect password, please try again");
+      password = sc.nextLine();
+    }
+  }
+}
